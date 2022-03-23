c99dfe1412f193c2e1bc13737bc32f11f8ff9a5f
shashi priya mishra <shashipriyamishra@gmail.com>
2022-03-23 13:19:49 +0530

    fix: update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import Head from "next/head"
+import Layout, { siteTitle } from "../components/layout"
+import utilStyles from "../styles/utils.module.css"
+import { getSortedPostsData } from "../lib/posts"
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
   return {
     props: {
-      allPostsData
-    }
+      allPostsData,
+    },
   }
 }
 
@@ -20,13 +20,13 @@ export default function Home({ allPostsData }) {
       {/* Add this <section> tag below the existing <section> tag */}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
-        <p>Hello my name is shashi Priya Mishra, I am a frontend developer with 8 years of experience and i worked on technologies like
-          HTML5, CSS, JAVASCRIPT, ANGULAR, REACT, NEXT.JS, TYPESCRIPT. I am currently living in bangalore 
-        </p>
         <p>
-          Contact me on - 9871454936
-          email- shashipriyamishra@gmail.com
+          Hello my name is shashi Priya Mishra , I am a frontend developer with
+          8 years of experience and i worked on technologies like HTML5, CSS,
+          JAVASCRIPT, ANGULAR, REACT, NEXT.JS, TYPESCRIPT. I am currently living
+          in bangalore
         </p>
+        <p>Contact me on - 9871454936 email- shashipriyamishra@gmail.com</p>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
